2f348270081598a287526963d55ecb0c81096cb6
w g <wgaul102@uottawa.ca>
2020-10-05 23:19:26 -0400

    Add contributor files

--- components/centraldashboard/public/components/manage-users-view-contributor.js
+++ components/centraldashboard/public/components/manage-users-view-contributor.js
@@ -6,17 +6,13 @@ import '@polymer/paper-toast/paper-toast.js';
 import '@polymer/paper-ripple/paper-ripple.js';
 import '@polymer/paper-item/paper-icon-item.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
-import '@vaadin/vaadin-grid/vaadin-grid.js';
-import '@vaadin/vaadin-grid/vaadin-grid-selection-column.js';
-import '@vaadin/vaadin-grid/vaadin-grid-sort-column.js';
 
 import {html, PolymerElement} from '@polymer/polymer';
 
 import './resources/paper-chip.js';
 import './resources/md2-input/md2-input.js';
-import './manage-users-view-contributor.js';
-import css from './manage-users-view.css';
-import template from './manage-users-view.pug';
+import css from './manage-users-view-contributor.css';
+import template from './manage-users-view-contributor.pug';
 import utilitiesMixin from './utilities-mixin.js';
 
 export class ManageUsersView extends utilitiesMixin(PolymerElement) {
@@ -33,7 +29,6 @@ export class ManageUsersView extends utilitiesMixin(PolymerElement) {
     static get properties() {
         return {
             user: {type: String, value: 'Loading...'},
-            isClusterAdmin: {type: Boolean, value: false},
             namespaces: Array,
             ownedNamespace: {type: Object, value: () => ({})},
             newContribEmail: String,
@@ -48,40 +43,14 @@ export class ManageUsersView extends utilitiesMixin(PolymerElement) {
         super.ready();
         this.contributorInputEl = this.$.ContribEmail;
     }
-    /**
-     * Returns namespaces and roles
-     * @param {[object]} ns Namespaces array.
-     * @return {[string, [string]]} rows for namespace table.
-     */
-    nsBreakdown(ns) {
-        const {ownedNamespace, namespaces} = this;
-        if (!ownedNamespace || !namespaces) return;
-        const arr = [];
-        for (let i = 0; i < namespaces.length; i++) {
-            arr.push(
-                [namespaces[i].namespace,
-                    this.uppercaseFirst(namespaces[i].role)],
-            );
-        }
-        return arr;
-    }
-
-    /**
-     * Used to uppercase the first letter of the role name
-     * @param {string} string A string
-     * @return {string} A string where the first character is capitalized
-     */
-    uppercaseFirst(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
 
     /**
      * Triggers an API call to create a new Contributor
      */
-    addNewContrib(e) {
+    addNewContrib() {
         // Need to call the api directly here.
         const api = this.$.AddContribAjax;
-        api.body = {contributor: e.target.$.inputElement.value};
+        api.body = {contributor: this.newContribEmail};
         api.generateRequest();
     }
     /**
@@ -137,24 +106,6 @@ export class ManageUsersView extends utilitiesMixin(PolymerElement) {
         const error = this._isolateErrorFromIronRequest(e);
         this.contribError = error;
         this.$.ContribError.show();
-    }
-    /**
-     * Iron-Ajax error handler for getContributors
-     * @param {IronAjaxEvent} e
-     */
-    onAllNamespaceFetchError(e) {
-        const error = this._isolateErrorFromIronRequest(e);
-        this.allNamespaceError = error;
-        this.$.AllNamespaceError.show();
-    }
-    /**
-     * [ComputedProp] Should the ajax call for all namespaces run?
-     * @param {object} ownedNamespace
-     * @param {boolean} isClusterAdmin
-     * @return {boolean}
-     */
-    shouldFetchAllNamespaces(ownedNamespace, isClusterAdmin) {
-        return isClusterAdmin && !this.empty(ownedNamespace);
     }
 }
 
