c75680ea305531ce73a2fc2d9d4cd5e8558df6e5
Bryan Paget <bryanpaget@pm.me>
2022-09-16 14:24:22 -0400

    re-added readiness probe

--- components/notebook-controller/controllers/notebook_controller.go
+++ components/notebook-controller/controllers/notebook_controller.go
@@ -476,6 +476,20 @@ func generateStatefulSet(instance *v1beta1.Notebook) *appsv1.StatefulSet {
 	}
 	// End AAW addition.
 
+	// Begin AAW Addition
+	// Add readiness probe
+	if container.ReadinessProbe == nil {
+		container.ReadinessProbe = &corev1.Probe{
+			Handler: corev1.Handler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path: "/notebook/" + instance.Namespace + "/" + instance.Name,
+					Port: intstr.FromInt(DefaultContainerPort),
+				},
+			},
+		}
+	}
+	// End AAW Addition
+
 	setPrefixEnvVar(instance, container)
 
 	// For some platforms (like OpenShift), adding fsGroup: 100 is troublesome.
