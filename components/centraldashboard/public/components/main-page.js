2e12cdda73454f190dd9a532f3bb875a768135a4
Wendy V Gaultier <wvgaultier@gmail.com>
2024-01-12 15:39:55 +0000

    Test Next Button

--- components/centraldashboard/public/components/main-page.js
+++ components/centraldashboard/public/components/main-page.js
@@ -657,6 +657,21 @@ export class MainPage extends mixinBehaviors([AppLocalizeBehavior], utilitiesMix
             'lang': this.language,
         };
     }
+
+    async nextPage() {
+        const APICreateDefault = this.$.CreateDefaultNotebook;
+
+        await APICreateDefault.generateRequest().completes.catch((e) => e);
+        await this.sleep(1); // So the errors and callbacks can schedule
+        if (this.error && this.error.response) {
+            if (this.error.response.error) {
+                this.set('error', {response: {
+                    error: 'registrationPage.errCreateNotebook',
+                    namespace: this.namespace,
+                }});
+            }
+        }
+    }
 }
 
 window.customElements.define('main-page', MainPage);
