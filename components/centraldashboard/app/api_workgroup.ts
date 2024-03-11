d6dfe65ec335eb4b1a128ee293cc0973e0d21a94
Wendy V Gaultier <wvgaultier@gmail.com>
2024-03-11 19:20:55 +0000

    Remove manage-contributors

--- components/centraldashboard/app/api_workgroup.ts
+++ components/centraldashboard/app/api_workgroup.ts
@@ -397,11 +397,12 @@ export class WorkgroupApi {
                 });
             }
         })
-        .post('/add-contributor/:namespace', async (req: Request, res: Response) => {
-            this.handleContributor('create', req, res);
-        })
-        .delete('/remove-contributor/:namespace', async (req: Request, res: Response) => {
-            this.handleContributor('remove', req, res);
-        });
+        // As part of 2.0, remove the functionnality to add or remove contributors
+        // .post('/add-contributor/:namespace', async (req: Request, res: Response) => {
+        //     this.handleContributor('create', req, res);
+        // })
+        // .delete('/remove-contributor/:namespace', async (req: Request, res: Response) => {
+        //     this.handleContributor('remove', req, res);
+        // });
     }
 }
--- components/centraldashboard/public/components/manage-users-view-contributor.js
+++ components/centraldashboard/public/components/manage-users-view-contributor.js
@@ -48,24 +48,6 @@ export class ManageUsersViewContributor extends mixinBehaviors([AppLocalizeBehav
     }
 
     /**
-     * Triggers an API call to create a new Contributor
-     */
-    addNewContrib() {
-        // Need to call the api directly here.
-        const api = this.$.AddContribAjax;
-        api.body = {contributor: this.newContribEmail};
-        api.generateRequest();
-    }
-    /**
-     * Triggers an API call to remove a Contributor
-     * @param {Event} e
-     */
-    removeContributor(e) {
-        const api = this.$.RemoveContribAjax;
-        api.body = {contributor: e.model.item};
-        api.generateRequest();
-    }
-    /**
      * Takes an event from iron-ajax and isolates the error from a request that
      * failed
      * @param {IronAjaxEvent} e
@@ -75,40 +57,7 @@ export class ManageUsersViewContributor extends mixinBehaviors([AppLocalizeBehav
         const bd = e.detail.request.response||{};
         return bd.error || e.detail.error || e.detail;
     }
-    /**
-     * Iron-Ajax response / error handler for addNewContributor
-     * @param {IronAjaxEvent} e
-     */
-    handleContribCreate(e) {
-        if (e.detail.error) {
-            const error = this._isolateErrorFromIronRequest(e);
-            if (error.search('fdi') === -1) {
-                this.contribCreateError =
-                    'manageUsersViewContributor.errorCreateGeneral';
-                return;
-            } else {
-                this.contribCreateError =
-                    'manageUsersViewContributor.errorCreateFDI';
-                return;
-            }
-        }
-        this.contributorList = e.detail.response;
-        this.newContribEmail = this.contribCreateError = '';
-    }
-    /**
-     * Iron-Ajax response / error handler for removeContributor
-     * @param {IronAjaxEvent} e
-     */
-    handleContribDelete(e) {
-        if (e.detail.error) {
-            // const error = this._isolateErrorFromIronRequest(e);
-            this.contribCreateError =
-                'manageUsersViewContributor.errorDeleteGeneral';
-            return;
-        }
-        this.contributorList = e.detail.response;
-        this.newContribEmail = this.contribCreateError = '';
-    }
+
     /**
      * Iron-Ajax error handler for getContributors
      * @param {IronAjaxEvent} e
