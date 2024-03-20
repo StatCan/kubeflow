fdc5402a2965f24d03703505863d92226be18f2e
Wendy Gaultier <wvgaultier@gmail.com>
2024-03-20 20:00:23 +0000

    Update links and items (#194)
    * Update links and items
    
    * Update community chat link
    
    * updated cypress run command to use edge instead of firefox
    
    ---------
    
    Co-authored-by: Mathis Marcotte <mathis.marcotte@statcan.gc.ca>

--- components/centraldashboard/cypress/e2e/landing-page.cy.ts
+++ components/centraldashboard/cypress/e2e/landing-page.cy.ts
@@ -16,7 +16,7 @@ describe('Landing Page', () => {
   it('should access the landing page', ()=>{
     cy.get('main-page').shadow().find('landing-page').should('exist');
     cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable > h2').should('have.text', 'Welcome');
-    cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable > aside').find('a').should('exist').and('have.prop', 'href', 'https://statcan-aaw.slack.com/');
+    cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable > aside').find('a').should('exist').and('have.prop', 'href', 'https://business-transformation.pages.cloud.statcan.ca/aaw/aaw/en/');
     cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable').find('md2-input#Email').should('exist').and('have.prop', 'value', 'user.name@email.com');
     cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable').find('md2-input#Namespace').should('exist').and('have.prop', 'value', 'user-name');
   });
