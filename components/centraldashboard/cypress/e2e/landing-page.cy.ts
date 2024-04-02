f8c7cea7e27bc35058023cf0c2d1855474e1ce05
Wendy V Gaultier <wvgaultier@gmail.com>
2024-04-02 19:02:10 +0000

    fix link to doc

--- components/centraldashboard/cypress/e2e/landing-page.cy.ts
+++ components/centraldashboard/cypress/e2e/landing-page.cy.ts
@@ -16,7 +16,7 @@ describe('Landing Page', () => {
   it('should access the landing page', ()=>{
     cy.get('main-page').shadow().find('landing-page').should('exist');
     cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable > h2').should('have.text', 'Welcome');
-    cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable > aside').find('a').should('exist').and('have.prop', 'href', 'https://business-transformation.pages.cloud.statcan.ca/zone/aaw/en/');
+    cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable > aside').find('a').should('exist').and('have.prop', 'href', 'https://business-transformation.pages.cloud.statcan.ca/aaw/zone/en/');
     cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable').find('md2-input#Email').should('exist').and('have.prop', 'value', 'user.name@statcan.gc.ca');
     cy.get('main-page').shadow().find('landing-page').shadow().find('#MainCard > neon-animatable').find('md2-input#Namespace').should('exist').and('have.prop', 'value', 'user-name');
   });
