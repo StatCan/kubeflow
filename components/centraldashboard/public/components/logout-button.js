42869201f677e2f52908c9258f048f88b8aeefb1
Wendy Gaultier <wendy.gaultier@statcan.gc.ca>
2025-01-21 20:19:30 +0000

    fix quote

--- components/centraldashboard/public/components/logout-button.js
+++ components/centraldashboard/public/components/logout-button.js
@@ -16,7 +16,7 @@ export class LogoutButton extends PolymerElement {
             <a href$="{{logoutUrl}}" on-tap="logout">
                 <paper-button id="logout-button">
                     <iron-icon icon="kubeflow:logout" 
-                               title="{{localize(landingPage.btnLogout")}}">
+                               title="{{localize('landingPage.btnLogout')}}">
                     </iron-icon>
                 </paper-button>
             </a>
