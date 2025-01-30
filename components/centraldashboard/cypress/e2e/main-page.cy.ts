a28eea9c225c5a97d78be4febfdb4f834619b9d9
Wendy Gaultier <wendy.gaultier@statcan.gc.ca>
2025-01-30 18:44:53 +0000

    temp fix

--- components/centraldashboard/cypress/e2e/main-page.cy.ts
+++ components/centraldashboard/cypress/e2e/main-page.cy.ts
@@ -25,7 +25,7 @@ describe('Main Page', () => {
     cy.get('main-page').shadow().find('dashboard-view').should('exist');
     // create new notebook link
     cy.get('main-page').shadow().find('dashboard-view').shadow().find('paper-card#Quick-Links').should('exist');
-    cy.get('main-page').shadow().find('dashboard-view').shadow().find('paper-card#Quick-Links').find('iframe-link').should('exist').and('have.length', 1).and('have.prop', 'href', '/en/new?ns={ns}');
+    cy.get('main-page').shadow().find('dashboard-view').shadow().find('paper-card#Quick-Links').find('iframe-link').should('exist').and('have.length', 1).and('have.prop', 'href', '/en/new');
     cy.get('main-page').shadow().find('dashboard-view').shadow().find('paper-card#Quick-Links').find('iframe-link').find('div.header').should('have.text', 'Create a new Notebook server');
     // documentation links
     cy.get('main-page').shadow().find('dashboard-view').shadow().find('paper-card#Documentation').should('exist');
