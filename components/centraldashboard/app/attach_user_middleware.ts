4ebd7ad2dec1a53633aec2788d5103bc263bb178
Mathis Marcotte <mathis.marcotte@canada.ca>
2025-03-31 18:50:59 +0000

    updated debug logs

--- components/centraldashboard/app/attach_user_middleware.ts
+++ components/centraldashboard/app/attach_user_middleware.ts
@@ -9,7 +9,7 @@ export function attachUser(
   return (req: Request, _: Response, next: NextFunction) => {
     let email = 'anonymous@kubeflow.org';
     let auth: User.AuthObject;
-    console.log("attachUSerReq", req.headers, req.authHeaders);
+    console.log("attachUSerReq", req.headers);
     if (userIdHeader && req.header(userIdHeader)) {
       email = req.header(userIdHeader).slice(userIdPrefix.length);
       auth = {[userIdHeader]: req.header(userIdHeader)};
