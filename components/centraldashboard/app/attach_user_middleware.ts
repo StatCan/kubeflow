c5abdbf40629b962f280e7615307d6a0032d67d5
Mathis Marcotte <mathis.marcotte@canada.ca>
2025-03-31 13:54:01 +0000

    added semi-colons

--- components/centraldashboard/app/attach_user_middleware.ts
+++ components/centraldashboard/app/attach_user_middleware.ts
@@ -9,7 +9,7 @@ export function attachUser(
   return (req: Request, _: Response, next: NextFunction) => {
     let email = 'anonymous@kubeflow.org';
     let auth: User.AuthObject;
-    console.log("attachUSerReq", req)
+    console.log("attachUSerReq", req);
     if (userIdHeader && req.header(userIdHeader)) {
       email = req.header(userIdHeader).slice(userIdPrefix.length);
       auth = {[userIdHeader]: req.header(userIdHeader)};
