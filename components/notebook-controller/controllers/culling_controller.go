e47d550f7669a2e1c1c4655e01b77164c22f8f5a
Mathis Marcotte <mathis.marcotte@canada.ca>
2025-05-01 14:08:28 +0000

    Revert "test commit to trigger ci"
    This reverts commit 474714941133e2f032234661a372348e752be20b.

--- components/notebook-controller/controllers/culling_controller.go
+++ components/notebook-controller/controllers/culling_controller.go
@@ -59,6 +59,7 @@ const (
 )
 
 // Each kernel of the Notebook Server has a status.
+// KernelStatus struct:
 type KernelStatus struct {
 	ID             string `json:"id"`
 	Name           string `json:"name"`
