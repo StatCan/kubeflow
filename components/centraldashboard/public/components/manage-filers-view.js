bd29a981e70428bef6b93fbec54d1b2d3afbb8b3
Mathis Marcotte <84033116+mathis-marcotte@users.noreply.github.com>
2025-08-05 13:24:52 +0000

    feat(centraldashboard): Add instructions to manage filers page (#260)
    * added intructions card for manage filers page
    
    * added translations for new manage filers instructions
    
    * updated language and made the instructions collapsing

--- components/centraldashboard/public/components/manage-filers-view.js
+++ components/centraldashboard/public/components/manage-filers-view.js
@@ -5,6 +5,8 @@ import '@polymer/paper-ripple/paper-ripple.js';
 import '@polymer/paper-item/paper-icon-item.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
 import '@polymer/paper-spinner/paper-spinner-lite.js';
+import '@polymer/paper-card/paper-card.js';
+import '@polymer/iron-collapse/iron-collapse.js';
 
 // eslint-disable-next-line max-len
 import {AppLocalizeBehavior} from '@polymer/app-localize-behavior/app-localize-behavior.js';
@@ -51,6 +53,18 @@ export class ManageFilersView extends mixinBehaviors([AppLocalizeBehavior], util
         return filersLoading ||
             existingSharesLoading ||
             requestingSharesLoading;
+    }
+
+    /**
+     * Determines if a shares object is empty
+     * @param {Object} userShares user filer shares object
+     * @return {boolean}
+     */
+    isEmpty(userShares) {
+        if (userShares == null) {
+            return true;
+        }
+        return Object.keys(userShares).length === 0;
     }
 
     /**
@@ -271,6 +285,16 @@ export class ManageFilersView extends mixinBehaviors([AppLocalizeBehavior], util
     */
     getRowspan(items) {
         return items.length;
+    }
+
+    toggle() {
+        this.$.collapseButton.classList.toggle('active');
+        this.$.collapse.toggle();
+    }
+
+    getActiveClass(userShares) {
+        return userShares == null || Object.keys(userShares).length === 0 ?
+            'active' : '';
     }
 }
 
