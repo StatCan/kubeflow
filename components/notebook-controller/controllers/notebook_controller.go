7d97514c8c971bceafcf6c0ece0c02ea7a2602fa
Mathis Marcotte <mathis.marcotte@canada.ca>
2025-10-21 19:51:43 +0000

    fixed debug

--- components/notebook-controller/controllers/notebook_controller.go
+++ components/notebook-controller/controllers/notebook_controller.go
@@ -758,7 +758,7 @@ func (r *NotebookReconciler) reconcileAuthorizationPolicy(instance *v1beta1.Note
 		instance.Namespace), Namespace: instance.Namespace}, foundAuthPol)
 	//TESTING
 	val, _ := foundAuthPol.MarshalJSON()
-	log.Info("test debug", string(val), "err", err.Error())
+	log.Info("test debug", "authpol", string(val), "err", err.Error())
 
 	if err != nil && apierrs.IsNotFound(err) {
 		log.Info("Creating authorization policy", "namespace", instance.Namespace, "name",
