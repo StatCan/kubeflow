3245d1be00f4f1edb33cfa53b952b17f0d6194af
Mathis Marcotte <84033116+mathis-marcotte@users.noreply.github.com>
2025-12-01 11:19:58 -0500

    fix(culler): commented out check for jupyter kernel activity (#265)
    * fix(culler):commented out check for jupyter kernel activity
    
    * chore: trigger build process
    
    * added comment

--- components/notebook-controller/controllers/culling_controller.go
+++ components/notebook-controller/controllers/culling_controller.go
@@ -334,13 +334,15 @@ func updateNotebookLastActivityAnnotation(meta *metav1.ObjectMeta, log logr.Logg
 	updated := false
 
 	nm, ns := meta.GetName(), meta.GetNamespace()
-	kernels := getNotebookApiKernels(nm, ns, log)
-	if kernels != nil && len(kernels) > 0 {
-		updateTimestampFromKernelsActivity(meta, kernels, log, &updated)
-		if updated {
-			return
-		}
-	}
+
+	// zone: Commented this out as the api call to get kernel activity for a notebook was constantly failing
+	// kernels := getNotebookApiKernels(nm, ns, log)
+	// if kernels != nil && len(kernels) > 0 {
+	// 	updateTimestampFromKernelsActivity(meta, kernels, log, &updated)
+	// 	if updated {
+	// 		return
+	// 	}
+	// }
 
 	cpuQuery := fmt.Sprintf("sum by(container) (node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace=\"%s\", container=\"%s\"})",
 		ns, nm)
