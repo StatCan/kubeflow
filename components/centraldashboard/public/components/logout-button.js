85203486bb4f8c384d33f93741e32babe326f5a6
Mathis Marcotte <84033116+mathis-marcotte@users.noreply.github.com>
2026-06-12 15:03:03 -0400

    Revert "fix(centraldashboard): reverted logout button back to doing a POST re…"
    This reverts commit 076ce969e4df53ef019aba230cb7353f5d3c06e1.

--- components/centraldashboard/public/components/logout-button.js
+++ components/centraldashboard/public/components/logout-button.js
@@ -1,85 +1,33 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-
-import '@polymer/iron-ajax/iron-ajax.js';
 import '@polymer/paper-button/paper-button.js';
-import '@polymer/paper-icon-button/paper-icon-button.js';
-import '@polymer/iron-icons/iron-icons.js';
+import css from './logout-button.css';
 
 /**
  * Logout button component.
  * Handles the logout requests and post-logout redirects.
  *
- * ZONE: Sends a POST request to the oidc-authservice for logout.
+ * AAW: Not using this compoent and the logoutURL env param. Doesn't work.
+ * https://github.com/StatCan/kubeflow/issues/152
  */
-
 export class LogoutButton extends PolymerElement {
     static get template() {
-        return html`
-            <paper-icon-button 
-                id="logout-button" 
-                icon="kubeflow:logout" 
-                on-tap="logout"
-                title="{{localize('landingPage.btnLogout')}}"
-            >
-            </paper-icon-button>
-            <iron-ajax
-                    id='logout'
-                    url$='{{logoutUrl}}'
-                    method='post'
-                    handle-as='json'
-                    headers='{{headers}}'
-                    on-response='_postLogout'>
-            </iron-ajax>
-        `;
-    }
-
-    static get properties() {
-        return {
-            headers: {
-                type: Object,
-                computed: '_setHeaders()',
-            },
-            logoutUrl: {
-                type: String,
-            },
-        };
+        return html([`
+            <style>${css.toString()}</style>
+            <a href$="{{logoutUrl}}" on-tap="logout">
+                <paper-button id="logout-button">
+                    <iron-icon icon="kubeflow:logout" 
+                               title="{{localize('landingPage.btnLogout')}}">
+                    </iron-icon>
+                </paper-button>
+            </a>
+        `]);
     }
 
     /**
-     * After successful logout, redirects user to `afterLogoutURL`,
-     * received from the backend.
-     *
-     * @param {{Event}} event
-     * @private
-     */
-    _postLogout(event) {
-        window.location.replace(event.detail.response['afterLogoutURL']);
-    }
-
-    /**
-     * Call logout endpoint.
+     * Set current page to logoutURL.
      */
     logout() {
-        // call iron-ajax
-        this.$.logout.generateRequest();
-    }
-
-    /**
-     * Set 'Authorization' header based on the existing cookie.
-     * Currently, the logout method only accepts authorization header, see:
-     * https://github.com/arrikto/oidc-authservice/blob/master/server.go#L386
-     *
-     * @return {{Object}} headers
-     * @private
-     */
-    _setHeaders() {
-        const cookie = ('; ' + document.cookie)
-            .split(`; authservice_session=`)
-            .pop()
-            .split(';')[0];
-        return {
-            'Authorization': `Bearer ${cookie}`,
-        };
+        window.top.location.href = this.logoutUrl;
     }
 }
 
