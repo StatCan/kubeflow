4834b80432d59aac5650bb8be14b5f7c346f1226
Mathis Marcotte <mathis.marcotte@canada.ca>
2026-09-11 17:45:21 +0000

    added style policy

--- components/centraldashboard/app/server.ts
+++ components/centraldashboard/app/server.ts
@@ -66,6 +66,7 @@ async function main() {
           baseUri: ["'none'"],
           objectScr: ["'none'"],
           scriptSrc: ["'self'", (req, res) => `'nonce-${(res as Response).locals.cspNonce}'`],
+          styleSrc: ["'self'", (req, res) => `'nonce-${(res as Response).locals.cspNonce}'`],
         },
       },
     }),
